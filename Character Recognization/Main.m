4c6e039dc7106ccd73e2ffaaf0e1dcf2e2513057
Miao Wang <wangnk2012@gmail.com>
2019-04-25 15:53:40 -0400

    Update Main.m

--- Character Recognization/Main.m
+++ Character Recognization/Main.m
@@ -29,11 +29,9 @@ costFunction = @(p) nnCostFunction(p, input_layer_size, hidden_layer_size, num_l
 [nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
 
 % Obtain Theta1 and Theta2 back from nn_params
-Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
-                 hidden_layer_size, (input_layer_size + 1));
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), hidden_layer_size, (input_layer_size + 1));
 
-Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
-                 num_labels, (hidden_layer_size + 1));
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), num_labels, (hidden_layer_size + 1));
 
 %% Implement Predict
 pred = predict(Theta1, Theta2, X);
